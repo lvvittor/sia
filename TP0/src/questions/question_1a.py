f28687d69b321356f9d3dc10f1b2e678ee92d11d
Ezequiel Rodriguez <learodriguez@itba.edu.ar>
2023-03-12 19:59:34 -0300

    adjust structure modularizing raw data extraction from experiment

--- TP0/src/questions/question_1a.py
+++ TP0/src/questions/question_1a.py
@@ -1,24 +1,9 @@
-from src.catching import attempt_catch
-from src.pokemon import PokemonFactory, StatusEffect
+from src.questions.helpers import build_dataframe_q1
 from IPython.display import display
-import pandas as pd
-import json
-
-POKEBALLS = ["pokeball", "fastball", "ultraball", "heavyball"]
 
 def question_1_a(pokemon_factory, config):
-    attempts = config["1_a"]["catch_attempts"]
-    df = pd.DataFrame(columns=["name", "pokeball", "catched", "attempts"])
-    for pokemon in pokemon_factory.get_available_pokemons():
-        new_pokemon = pokemon_factory.create(pokemon, config["1_a"]["pokemons_level"], StatusEffect.NONE, config["1_a"]["pokemons_health"]/100)
-        for pokeball in POKEBALLS:
-            catched = 0
-            for _ in range(attempts):
-                attempt, rate = attempt_catch(new_pokemon, pokeball)
-                if attempt:
-                    catched += 1
-            df.loc[len(df)] = [pokemon, pokeball, catched, attempts]
-    df["accuracy"] = df.catched / df.attempts
+    df = build_dataframe_q1(pokemon_factory, config)
     df = df.groupby("pokeball").agg({"accuracy": ["mean", "std"]}).reset_index()
     df.columns = df.columns.map("_".join)
     display(df)
+    return df
