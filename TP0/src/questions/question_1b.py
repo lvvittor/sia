0df5fca75cb72b189854ec1eb4d11a0cab52d9c7
Ezequiel <eperez@itba.edu.ar>
2023-03-14 19:24:37 -0300

    feat: question 1b

--- TP0/src/questions/question_1b.py
+++ TP0/src/questions/question_1b.py
@@ -6,6 +6,13 @@ import matplotlib.pyplot as plt
 
 def question_1_b(pokemon_factory, config):
     df = build_dataframe_q1(pokemon_factory, config)
+
+    pokeballs = {}
+
+    # get accuracy for each pokeball
+    for pokeball in df["pokeball"].unique():
+        df_pokeball = df[df["pokeball"] == pokeball]
+        pokeballs[pokeball] = df_pokeball["accuracy"].values.tolist()
     
     pokemons = pokemon_factory.get_available_pokemons()
 
@@ -13,32 +20,27 @@ def question_1_b(pokemon_factory, config):
     barWidth = 0.25
     fig = plt.subplots(figsize =(12, 8))
     
-    # set height of bar
-    POKEBALL_PROBS = [12, 30, 1, 8, 22]
-    FASTBALL_PROBS = [28, 6, 16, 5, 10]
-    ULTRABALL_PROBS = [29, 3, 24, 25, 17]
-    HEAVYBALL_PROBS = [29, 3, 24, 25, 17]
-    
     # Set position of bar on X axis
-    br1 = np.arange(len(POKEBALL_PROBS))
+    br1 = np.arange(len(pokeballs["pokeball"]))
+    br1 = [x + 0.1*x for x in br1] # add some space between pokemons
     br2 = [x + barWidth for x in br1]
     br3 = [x + barWidth for x in br2]
     br4 = [x + barWidth for x in br3]
     
     # Make the plot
-    plt.bar(br1, POKEBALL_PROBS, color ='r', width = barWidth,
+    plt.bar(br1, pokeballs["pokeball"], color ='r', width = barWidth,
             edgecolor ='grey', label ='Pokeball')
-    plt.bar(br2, FASTBALL_PROBS, color ='g', width = barWidth,
+    plt.bar(br2, pokeballs["fastball"], color ='g', width = barWidth,
             edgecolor ='grey', label ='Fastball')
-    plt.bar(br3, ULTRABALL_PROBS, color ='b', width = barWidth,
+    plt.bar(br3, pokeballs["ultraball"], color ='b', width = barWidth,
             edgecolor ='grey', label ='Ultraball')
-    plt.bar(br4, HEAVYBALL_PROBS, color ='y', width = barWidth,
+    plt.bar(br4, pokeballs["heavyball"], color ='y', width = barWidth,
             edgecolor ='grey', label ='Heavyball')
     
     # Adding Xticks
     plt.xlabel('Pokemons', fontweight ='bold', fontsize = 15)
     plt.ylabel('Pokeballs\' catching probability', fontweight ='bold', fontsize = 15)
-    plt.xticks([r + barWidth for r in range(len(POKEBALL_PROBS))], pokemons)
+    plt.xticks([r + barWidth*1.5 + 0.1*r for r in range(len(pokeballs["pokeball"]))], pokemons) # put the name in between the bars
     
     plt.legend()
     plt.savefig("./graphs/q_1_b.png")
