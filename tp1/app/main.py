abe078477312169de07d60ce3a7f3fc084d2e8d9
Lucas Vittor <lvvittor@gmail.com>
2023-03-18 22:10:46 -0300

    feat: add config file

--- tp1/app/main.py
+++ tp1/app/main.py
@@ -2,6 +2,6 @@ from services import BoardGeneratorService
 from settings import settings
 
 if __name__ == "__main__":
-    board_generator = BoardGeneratorService(settings.N, settings.M)
+    board_generator = BoardGeneratorService(settings.board.N, settings.board.M)
     board = board_generator.generate()
     print(board)
--- tp1/app/settings.py
+++ tp1/app/settings.py
@@ -1,7 +1,42 @@
-from pydantic import BaseSettings
+from typing import Any
+from pydantic import BaseSettings, BaseModel
+from pathlib import Path
+import json
+
+def json_config_settings_source(settings: BaseSettings) -> dict[str, Any]:
+    """
+    A simple settings source that loads variables from a JSON file
+    at the project's root.
+    """
+    encoding = settings.__config__.env_file_encoding
+    config_path = settings.__config__.config_path
+    return json.loads(Path(config_path).read_text(encoding))
+
+
+class BoardSettings(BaseModel):
+    M: int # number of colors
+    N: int # board of size NxN
 
 class Settings(BaseSettings):
-    M: int = 10 # number of colors
-    N: int = 10  # board of size NxN
+    board: BoardSettings
+
+    class Config:
+        env_file_encoding = 'utf-8'
+        config_path = 'tp1/config.json'
+
+        @classmethod
+        def customise_sources(
+            cls,
+            init_settings,
+            env_settings,
+            file_secret_settings,
+        ):
+            return (
+                init_settings,
+                json_config_settings_source,
+                env_settings,
+                file_secret_settings,
+            )
+
 
 settings = Settings()
