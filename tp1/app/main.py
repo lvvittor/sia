b4beafa8edf28dc4461c96eba35db721cdb9da62
gbaliarda <gbaliarda@itba.edu.ar>
2023-03-19 19:49:37 -0300

    feat: build state [WIP] Co-authored-by: Ezequiel-Rodriguez <learodriguez@itba.edu.ar>

--- tp1/app/main.py
+++ tp1/app/main.py
@@ -1,7 +1,6 @@
 from services import BoardGeneratorService, BoardService, BenchMarkService
 from settings import settings
 from algorithms.dfs import solve_dfs
-from region import RegionFactory
 import pandas as pd
 
 def solve_algorithm():
@@ -12,16 +11,17 @@ def solve_algorithm():
 if __name__ == "__main__":
     board_generator = BoardGeneratorService(settings.board.N, settings.board.M)
     board = board_generator.generate()
-    regions = RegionFactory.create(board)
-    print(board)
-    solve_algorithm()
-    board_service = BoardService(board)
+    # print(board)
 
-    with pd.option_context('display.max_rows', settings.board.N, 'display.max_columns', settings.board.N):
-        print(f"Board:")
-        print(board_service.get_board())
-        board_service.set_colored_board("test.png")
-        print(f"Board size: {board_service.get_board_size()}")
-        print(f"Board colors: {board_service.get_board_colors()}")
-        print(f"Board color count: {board_service.get_board_color_count()}")
+    # print(board.iterrows())
+    # solve_algorithm()
+    # board_service = BoardService(board)
+
+    # with pd.option_context('display.max_rows', settings.board.N, 'display.max_columns', settings.board.N):
+    #     print(f"Board:")
+    #     print(board_service.get_board())
+    #     board_service.set_colored_board("test.png")
+    #     print(f"Board size: {board_service.get_board_size()}")
+    #     print(f"Board colors: {board_service.get_board_colors()}")
+    #     print(f"Board color count: {board_service.get_board_color_count()}")
 
--- tp1/app/region.py
+++ tp1/app/region.py
@@ -1,11 +1,4 @@
 from __future__ import annotations 
-from enum import Enum
-from pandas import DataFrame
-
-class Color(Enum):
-    RED = "red"
-    GREEN = "green"
-    BLUE = "blue"
 
 class Region():
     def __init__(
@@ -15,46 +8,39 @@ class Region():
         cells: list[Cell],
         adjacents: list[Region]
     ):
-        self._color = color
-        self._id = id
-        self._cells = cells
-        self._adjacents = adjacents
-        
+        self.color = color
+        self.id = id
+        self.cells = cells
+        self.adjacents = adjacents
+
+    # Necesita un equals para comparar si dos regiones son iguales?
         
 
 class Cell():
     def __init__(
         self,
         visited: bool,
-        x: int,
-        y: int,
-    ):
-        self._visited = visited
-        self._x = x
-        self._y = y
-
-
-class RegionFactory:
-    @staticmethod
-    def create(board: DataFrame):
-        regions = []
-        RegionFactory.build_regions(0, board, 0, 0, regions, None, None)
-
-    @staticmethod
-    def build_regions(
-        board: DataFrame,
-        region_number: int,
-        x: int,
-        y: int,
-        regions: list[Region],
-        current_region: Region,
         color: int,
+        x: int,
+        y: int,
+        region_id: int
     ):
-        cell = Cell(x, y)
-        if (color != board.iat[x,y]):
-            current_region = Region(region_number, board.iat[x,y], [cell], [])
-            regions.append(current_region)
-            region_number += 1
+        self.visited = visited
+        self.color = color
+        self.x = x
+        self.y = y
+        self.region_id = region_id
+
+class State():
+    def __init__(
+        self,
+        regions: dict[int, Region],
+        # add parameters for heuristics
+    ):
+        self.regions = regions
+
+
+
         
         
         
