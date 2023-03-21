1e5409de439bb8829dc1e957b2476f2255c6ee09
gbaliarda <gbaliarda@itba.edu.ar>
2023-03-21 13:47:41 -0300

    chore: colors do not change after each iteration

--- tp1/app/main.py
+++ tp1/app/main.py
@@ -12,20 +12,21 @@ def solve_algorithm():
 if __name__ == "__main__":
     board_generator = BoardGeneratorService(settings.board.N, settings.board.M)
     board = board_generator.generate()
+    board_service = BoardService()
 
     i = 0
     while True:
         df = board_generator.dict_to_df(board.regions)
+        print(df)
 
-        # solve_algorithm()
-        board_service = BoardService(df)
-
-        with pd.option_context('display.max_rows', settings.board.N, 'display.max_columns', settings.board.N):
-            print(board_service.get_board())
+        # This prints will not work because i removed those methods as there is no DataFrame inside BoardService anymore.
+        # TODO: remove/change prints
+        # with pd.option_context('display.max_rows', settings.board.N, 'display.max_columns', settings.board.N):
+            # print(board_service.get_board())
         
-        print(f"Board [colors={board_service.get_board_colors()}, len(colors)={board_service.get_board_color_count()}]")
+        # print(f"Board [colors={board_service.get_board_colors()}, len(colors)={board_service.get_board_color_count()}]")
         
-        board_service.set_colored_board(f"test{i}.png")
+        board_service.set_colored_board(df, f"test{i}.png")
         i += 1
 
         new_color = input("What color do you want to change: ")
