219c65b0d2f833365f0aa3c3409d726ef308b796
gbaliarda <gbaliarda@itba.edu.ar>
2023-03-21 16:02:49 -0300

    chore: assemble state and dfs algorithm
    Co-authored-by: erodriguezzz <learodriguez@itba.edu.ar>
    Co-authored-by: EperezOk <eperez@itba.edu.ar>

--- tp1/app/algorithms/dfs.py
+++ tp1/app/algorithms/dfs.py
@@ -1,10 +1,13 @@
 from algorithms.solver import Solver
+from settings import settings
+from services.board_generator_service import BoardGeneratorService
+from services.board_service import BoardService
 
 # Ver ejemplo de uso al final del archivo
 
 class DFS(Solver):
-  def __init__(self, N, M):
-    super().__init__(N, M)
+  def __init__(self, state):
+    super().__init__(state)
 
 
   # Especifico de cada metodo de busqueda
@@ -13,24 +16,34 @@ class DFS(Solver):
     Busca una solucion recursivamente.
     Devuelve True si la encontro, False en caso contrario.
     """
-    if self.remaining_cells == 0:
+    if len(self.state.regions) == 1:
       self.solution_cost = cost # guardamos el costo de la solucion
       return True
     
     # Guardamos el estado actual del tablero en caso de que necesitemos rollbackear (i.e. actualizamos el color de toda la zona pero en realidad no habia vecinos nuevos)
-    board_copy = self.board.copy()
+    state_copy = self.state.copy()
     
     # Visitar vecinos de la zona
-    visited = self.visit_zone_neighbors(color)
+    # print(f"Cambiando de color a {color}")
+    expansions = self.expand_zone(color)
     
     # Si no se visitaron nuevas celdas, descartamos este camino
-    if visited == 0:
-      # print(f"Pincho con color {color}")
-      self.board = board_copy # rollback
+    if expansions == 0:
+      # print("Rollbackeando")
+      self.state = state_copy # rollback
       return False
     
+    # Imprimir estados intermedios
+    board_generator = BoardGeneratorService(settings.board.N, settings.board.M)
+    state_df = board_generator.dict_to_df(self.state.regions)
+    print()
+    print(state_df)
+    self.board_service.set_colored_board(state_df, f"dfs{cost}.png")
+    # print(f"Costo actual: {cost+1}")
+    print()
+    
     # Probamos con todos los colores hasta que alguno de ellos lleve a una solucion
-    for c in range(1, self.M+1):
+    for c in range(0, settings.board.M):
       if c != color and self.search(c, cost+1):
         return True
     
@@ -40,27 +53,30 @@ class DFS(Solver):
 
   def solve(self):
     # Probamos al inicio con todos los colores hasta que alguno de ellos lleve a una solucion
-    for c in range(1, self.M+1):
+    for c in range(0, settings.board.M):
       # print(f"Probando con color {c}")
       if self.search(c, 0):
         break
     
-    return self.board, self.solution_cost
+    return self.state, self.solution_cost
 
 
 # Ejemplo de uso (tamaño original: 14x14, 6 colores)
 if __name__ == "__main__":
-  N = 14 # tamaño del tablero
-  M = 6  # cantidad de colores
+  board_generator = BoardGeneratorService(settings.board.N, settings.board.M)
+  board = board_generator.generate()
+  board_service = BoardService()
 
-  dfs_solver = DFS(N, M)
+  dfs_solver = DFS(board)
 
   print("Tablero inicial:")
-  print(dfs_solver.board)
+  initial_df = board_generator.dict_to_df(board.regions)
+  print(initial_df)
 
   solution, cost = dfs_solver.solve()
 
+  solution_df = board_generator.dict_to_df(solution.regions)
   print("Tablero solucion:")
-  print(solution)
+  print(solution_df)
 
   print(f"Costo de la solucion: {cost}")
--- tp1/app/main.py
+++ tp1/app/main.py
@@ -1,37 +1,47 @@
+from region import State
 from services import BoardGeneratorService, BoardService, BenchMarkService
 from settings import settings
 from algorithms import DFS
 import pandas as pd
 
-def solve_algorithm():
+def solve_algorithm(state: State):
     match settings.algorithm:
         case "dfs":
-            dfs_solver = DFS(settings.board.N, settings.board.M)
-            board, cost = dfs_solver.solve()
+            dfs_solver = DFS(state)
+
+            initial_df = board_generator.dict_to_df(state.regions)
+            print(initial_df)
+
+            solution, cost = dfs_solver.solve()
+            solution_df = board_generator.dict_to_df(solution.regions)
+            print("Tablero solucion:")
+            print(solution_df)
+
+            print(f"Costo de la solucion: {cost}")
 
 if __name__ == "__main__":
     board_generator = BoardGeneratorService(settings.board.N, settings.board.M)
     board = board_generator.generate()
     board_service = BoardService()
+    solve_algorithm(board)
+    # i = 0
+    # while True:
+    #     df = board_generator.dict_to_df(board.regions)
+    #     print(df)
 
-    i = 0
-    while True:
-        df = board_generator.dict_to_df(board.regions)
-        print(df)
-
-        # This prints will not work because i removed those methods as there is no DataFrame inside BoardService anymore.
-        # TODO: remove/change prints
-        # with pd.option_context('display.max_rows', settings.board.N, 'display.max_columns', settings.board.N):
-            # print(board_service.get_board())
+    #     # This prints will not work because i removed those methods as there is no DataFrame inside BoardService anymore.
+    #     # TODO: remove/change prints
+    #     # with pd.option_context('display.max_rows', settings.board.N, 'display.max_columns', settings.board.N):
+    #         # print(board_service.get_board())
         
-        # print(f"Board [colors={board_service.get_board_colors()}, len(colors)={board_service.get_board_color_count()}]")
+    #     # print(f"Board [colors={board_service.get_board_colors()}, len(colors)={board_service.get_board_color_count()}]")
         
-        board_service.set_colored_board(df, f"test{i}.png")
-        i += 1
+    #     board_service.set_colored_board(df, f"test{i}.png")
+    #     i += 1
 
-        new_color = input("What color do you want to change: ")
+    #     new_color = input("What color do you want to change: ")
 
-        board = board_generator.update_state(int(new_color))
+    #     board = board_generator.update_state(int(new_color))
   
 
 
