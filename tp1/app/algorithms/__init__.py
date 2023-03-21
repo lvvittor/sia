3a483b04f6e00a845c385775873badecc89892ce
festevezz <festevez@itba.edu.ar>
2023-03-21 19:36:48 -0300

    feat: complete bfs implementation

--- tp1/app/algorithms/__init__.py
+++ tp1/app/algorithms/__init__.py
@@ -1,3 +1,4 @@
 from algorithms.dfs import DFS
+from algorithms.bfs import BFS
 
-__all__ = ["DFS"]
+__all__ = ["DFS", "BFS"]
--- tp1/app/algorithms/bfs.py
+++ tp1/app/algorithms/bfs.py
@@ -1,55 +1,76 @@
-from solver import Solver
+from algorithms.solver import Solver
+from settings import settings
 from services.board_generator_service import BoardGeneratorService
+from services.board_service import BoardService
 
 class BFS(Solver):
-    def __init__(self, N, M):
-        super().__init__(N, M)
+    def __init__(self, state):
+        super().__init__(state)
     
-    def search(self, board):
+    def search(self):
         """
         Busca una solucion iterativamente.
         Devuelve True si la encontro, False en caso contrario.
         """
-        
-        cost = 0
 
-        # put numbers from 0 to M in queue 
-        colors = [i for i in range(1, self.M+1)]
-        queue = [colors]
+        # all colors
+        colors = [i for i in range(0, settings.board.M)]
 
-        # amount of regions
-        regions = len(board.state.regions)
+        # append colors to color_queue
+        color_queue = []
+        for color in colors:
+            color_queue.append(color)
+
+        state_queue = [self.state]
+        current_state = None
         
-        while queue:
+        while color_queue:
             # get first color
-            color = queue.pop(0)
-            cost += 1
+            color = color_queue.pop(0)
+
+            # if we are checking the first color, then we are checking a new state
+            if color == 0:
+                current_state = state_queue.pop(0)
+                self.solution_cost += 1
 
             # try to update board state with that color
-            board.update_state(color)
+            expansions = self.expand_zone(color)
 
             # if no new regions were merged, discard this path
-            if len(board.state.regions) == regions:
-                board.undo_update()
-                cost -= 1
+            if expansions == 0:
+                self.state = current_state
                 continue
 
             # some regions were merged, check if we found a solution
-            if len(board.state.regions) == 1:
-                return True, cost
+            if len(self.state.regions) == 1:
+                return True
             
-            # if not, add new colors to queue
-            map(queue.append, colors)
+            # an expansion has been made, save the state and add colors to queue
+            state_queue.append(self.state)
+            for color in colors:
+                color_queue.append(color)
+
+            # print intermediate states
+            board_generator = BoardGeneratorService(settings.board.N, settings.board.M)
+            state_df = board_generator.dict_to_df(self.state.regions)
+            print()
+            print(state_df)
+            self.board_service.set_colored_board(state_df, f"bfs{self.solution_cost}-{color}.png")
+            print()
+
+            # rollback for next color check
+            self.state = current_state
         
         # no solution found
-        return False  
+        return False
 
     def solve(self):
         """
         Genera un tablero inicial y lo resuelve.
         """
-        board = BoardGeneratorService(self.N, self.M).generate()
-        return self.search(board)
+        if self.search():
+            return self.state, self.solution_cost
+        return None, None
 
 # Ejemplo de uso (tamaño original: 14x14, 6 colores)
 if __name__ == "__main__":
