fbcdd7be719cecfb499c02efcc86520237ca5d15
Ezequiel Rodriguez <learodriguez@itba.edu.ar>
2023-04-15 23:29:58 -0300

    modify loop to iterate fitness

--- tp2/app/selection.py
+++ tp2/app/selection.py
@@ -46,7 +46,7 @@ def ranking_selection(population, fitness, k):
     ranked = sorted(fitness, reverse=True)
     selected = []
 
-    for i in len(fitness):
+    for i in range(len(fitness)):
         selected[i] = (len(fitness) - ranked.index(fitness[i]) + 1)/len(fitness)
 
     return roulette_selection(population, selected, k)
