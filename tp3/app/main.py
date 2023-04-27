a4e3b3c52d8ee2943f95c5ad5703104c71735d68
Lucas Vittor <lvvittor@gmail.com>
2023-04-27 02:13:01 -0300

    chore: wip accuracy

--- tp3/app/main.py
+++ tp3/app/main.py
@@ -1,16 +1,12 @@
 import numpy as np
 
-from step_perceptron import StepPerceptron
+from simple_perceptron import SimplePerceptron
 from settings import settings
-from utils import logic_and
 
 if __name__ == "__main__":
-  inputs = np.array([[-1, 1], [1, -1], [-1, -1], [1, 1]])
-  # expected_outputs = np.vectorize(logic_and)(inputs)
-  expected_outputs = np.apply_along_axis(logic_and, axis=1, arr=inputs)
-  step_perceptron = StepPerceptron(settings.learning_rate, inputs, expected_outputs)
-
-  epochs = step_perceptron.train()
-  print(f"Finished at {epochs} epochs")
-
-  print("Outputs: ", step_perceptron.get_outputs())
+    # X = np.array([[-1, 1], [1, -1], [-1, -1], [1, 1]])
+    # y = np.array([-1, -1, -1, 1])
+    X = np.array([[-1, 1], [1, -1], [-1, -1], [1, 1]])
+    y = np.array([1, 1, -1, 1])
+    perceptron = SimplePerceptron(eta=settings.learning_rate, epochs=settings.epochs)
+    perceptron.fit(X, y)
---
+++ tp3/app/simple_perceptron.py
@@ -0,0 +1,79 @@
+import numpy as np
+import math
+
+
+class SimplePerceptron:
+    def __init__(self, eta: float = 0.1, epochs: int = 1000):
+        """Constructor method
+
+        Args:
+            eta (float): learning rate of the perceptron
+            epochs (int): maximum number of epochs to train the perceptron
+        """
+        self.eta = eta
+        self.epochs = epochs
+
+    def fit(self, X: np.ndarray, y: np.ndarray):
+        """Train the perceptron
+
+        Args:
+            X (np.ndarray): The input data of shape (n_samples, m_features)
+            y (np.ndarray): The expected output array of shape (n_samples,)
+
+        Returns:
+            int, bool: The number of epochs needed to train the perceptron, and whether the perceptron converged or not
+        """
+        weights = np.random.rand(
+            X.shape[1],
+        )  # Initialize weights
+
+        for epoch in range(self.epochs):
+            predicted = []
+            for i, sample in enumerate(X):
+                y_hat = self.predict(sample, weights)
+                predicted.append(y_hat)
+
+                for j, _ in enumerate(weights):
+                    delta = self.eta * (y[i] - y_hat)
+                    delta = delta * sample[j - 1]
+                    weights[j - 1] = weights[j - 1] + delta
+            print(f"delta={delta}, weights={weights}")
+            print(f"Epoch {epoch}: accuracy={self._accuracy(y, predicted)}")
+
+    def predict(self, X: np.ndarray, w: np.ndarray):
+        """Predict the output of the perceptron for a given input
+
+        Args:
+            X (np.ndarray): The data sample of shape (m_features,)
+            w (np.ndarray): The weights array of shape (m_features,)
+
+        Returns:
+            int: Return 1 if the perceptron predicts a positive output, -1 otherwise
+        """
+
+        return np.where(self._sum(X, w) >= 0, 1, -1)
+
+    def _sum(self, X: np.array, w: np.array):
+        """Compute the sum of the product of the inputs and the weights
+
+        Args:
+            X (np.array): The data sample of shape (m_features,)
+            w (np.array): The weights array of shape (m_features,)
+
+        Returns:
+            int: The sum of the product of the inputs and the weights
+        """
+        return np.sum(np.dot(X, np.transpose(w)))
+
+    def _accuracy(self, y: np.ndarray, y_hat: np.ndarray):
+        """Calculate the accuracy of predictions for a given epoch.
+
+        Args:
+            y (np.ndarray): actual array of shape (n_samples, )
+            y_hat (np.ndarray): predicted array of shape (n_samples,)
+
+        Returns:
+            float: The accuracy of the perceptron
+        """
+        print(y, y_hat)
+        return np.sum(y == y_hat) / y.shape[0]
